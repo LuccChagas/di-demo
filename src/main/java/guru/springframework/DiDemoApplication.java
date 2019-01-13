6d6410773ea2a278d2f0ba39cbbbf3a447c9799a
LuccChagas <luccaa.chagas23@gmail.com>
2019-01-13 17:28:32 -0200

    Initial commit

---
+++ src/main/java/guru/springframework/DiDemoApplication.java
@@ -0,0 +1,23 @@
+package guru.springframework;
+
+import guru.springframework.Controllers.MyController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import static org.springframework.boot.SpringApplication.run;
+
+@SpringBootApplication
+public class DiDemoApplication {
+
+    public static void main(String[] args) {
+
+     ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
+
+        MyController controller = (MyController) ctx.getBean("myController");
+
+        controller.hello();
+    }
+
+}
+
